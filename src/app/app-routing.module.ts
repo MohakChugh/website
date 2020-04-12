229dad69a65b404b7f237ecffd5813d0da7ee779
Mohak Chugh <mohakchugh@pop-os.localdomain>
2020-04-12 15:34:19 +0530

    project pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { IndexComponent } from './index/index.component';
 import { CvComponent } from './cv/cv.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { CitizensappComponent } from './projects/citizensapp/citizensapp.component';
+import { FarmersappComponent } from './projects/farmersapp/farmersapp.component';
+import { ContentPusherComponent } from './projects/content-pusher/content-pusher.component';
+import { PropertyManagementComponent } from './projects/property-management/property-management.component';
 
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'cv', component: CvComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'project', component: ProjectsComponent }
+  { path: 'project', component: ProjectsComponent } ,
+  { path: 'citizensapp', component: CitizensappComponent },
+  { path: 'farmersapp', component: FarmersappComponent },
+  { path: 'blogger', component: ContentPusherComponent },
+  { path: 'propertymanagement', component: PropertyManagementComponent }
 ];
 
 @NgModule({
