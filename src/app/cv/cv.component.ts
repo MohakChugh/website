0cb9a47bed8d9e41964febfcc48adf5cdecea211
Mohak Chugh <chumohak@amazon.com>
2022-01-26 03:02:16 +0530

    cv generate from html page added

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-cv',
@@ -10,6 +11,22 @@ export class CvComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  async generateCv() {
+    console.log('Generate CV Function called!');
+    const cvContent = document.getElementById('cv-content').innerHTML;
+    // html2pdf(cvContent);
+    const options = {
+      margin:       1,
+      filename:     'cv.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+    // save file as pdf
+    await html2pdf().from(cvContent).set(options).save();
+
   }
 
 }
